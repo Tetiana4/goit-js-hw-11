f6231fbe9dd136bfb20bbb4dfe1c99239811dff9
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-07-27 08:11:37 +0200

    Добавляет код

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import './sass/main.scss';
+// import photoCard from '../templates/photo-card';
+import Notiflix from "notiflix";
+import NewsApiService from './js/news-service';
+
+
+const newsApiService = new NewsApiService();
+
+console.log(newsApiService);
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    cardGallery: document.querySelector('.gallery'),
+    loadMoreButton: document.querySelector('.load-more'),
+}
+
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreButton.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+    e.preventDefault();
+
+    newsApiService.query = e.currentTarget.elements.searchQuery.value;
+    
+    if (newsApiService.query === '') {
+        return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    }
+
+    newsApiService.fetchImages();
+};
+
+function onLoadMore() {
+    newsApiService.fetchImages();
+}
