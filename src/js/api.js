277156c5a8492b7d95fe87138e6b389290895af1
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-08-03 21:59:39 +0200

    Добавляет файл

--- src/js/api.js
+++ src/js/api.js
@@ -1,1 +1,9 @@
+export { refs }
 
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    cardGallery: document.querySelector('.gallery'),
+    loadMoreButton: document.querySelector('.load-more'),
+    submitBtn: document.querySelector('.submit-btn'),
+}
